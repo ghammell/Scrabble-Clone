3c084be3328217f9dbc4ce895f7ec9d48edacc49
Gary Hammell <ghammell1@gmail.com>
2014-11-25 22:46:51 -0800

    prelim reset of words, moving letters back to hand

--- app/controllers/coordinates_controller.rb
+++ app/controllers/coordinates_controller.rb
@@ -11,9 +11,12 @@ class CoordinatesController < ApplicationController
 
   def reset_word
     session[:current_word].each {|hash| Coordinate.find(hash['id']).update_attribute('letter', '')}
-    @letters = session[:current_word].map {|hash| hash['letter']}
     @reset_ids = session[:current_word].map{|hash| hash['id']}
+    @letters = session[:current_word].map {|hash| ('A'..'Z').to_a.index(hash['letter'])}
+    @player = session[:player]
     session[:current_word] = []
+    p @letters
+    p @reset_ids
   end
 
   def update_word(coordinate)
