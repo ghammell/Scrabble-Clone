d8f560b661d9268c4afd787e692bead91566cecf
Gary Hammell <ghammell1@gmail.com>
2014-12-02 15:52:03 -0800

    comment out unused code, remove puts

--- app/helpers/coordinates_helper.rb
+++ app/helpers/coordinates_helper.rb
@@ -61,28 +61,28 @@ module CoordinatesHelper
       coordinates.sort_by {|coord| coord.vertical}.sort_by{|coord| coord.horizontal}
     end
 
-    def self.check_placement(coordinates)
-      coordinates.map {|coord| [coord.vertical, coord.horizontal]}
-      (1...coordinates.length).each do |index|
-        previous = [coordinates[index-1].vertical, coordinates[index-1].horizontal]
-        current = [coordinates[index].vertical, coordinates[index].horizontal]
-        return false if test_all(current, previous) == false
-      end
-      return true
-    end
+    # def self.check_placement(coordinates)
+    #   coordinates.map {|coord| [coord.vertical, coord.horizontal]}
+    #   (1...coordinates.length).each do |index|
+    #     previous = [coordinates[index-1].vertical, coordinates[index-1].horizontal]
+    #     current = [coordinates[index].vertical, coordinates[index].horizontal]
+    #     return false if test_all(current, previous) == false
+    #   end
+    #   return true
+    # end
 
-    def self.test_all(current, previous)
-      return true if test_vertical(current, previous) == true
-      return test_horizontal(current, previous)
-    end
+    # def self.test_all(current, previous)
+    #   return true if test_vertical(current, previous) == true
+    #   return test_horizontal(current, previous)
+    # end
 
-    def self.test_vertical(current, previous)
-      (current[0] == previous[0]) && (current[1] == previous[1] + 1)
-    end
+    # def self.test_vertical(current, previous)
+    #   (current[0] == previous[0]) && (current[1] == previous[1] + 1)
+    # end
 
-    def self.test_horizontal(current, previous)
-      (current[0] == previous[0] + 1) && (current[1] == previous[1])
-    end
+    # def self.test_horizontal(current, previous)
+    #   (current[0] == previous[0] + 1) && (current[1] == previous[1])
+    # end
   end
 
   def self.determine_droppable_coordinates(game_id)
