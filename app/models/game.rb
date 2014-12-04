84c3c4433ce188fccde12cf3c7d6348003de3828
Gary Hammell <ghammell1@gmail.com>
2014-12-03 20:24:34 -0800

    more styling for multiplier

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,11 +3,12 @@ class Game < ActiveRecord::Base
   has_many :coordinates
 
   def create_coordinates
+    mult_spots = [0, 3, 7, 10, 14]
     (0..14).each do |row|
       i = 0
       (0..14).each do |column|
         coordinate = self.coordinates.create(horizontal: row, vertical: column)
-        if coordinate.vertical % 2 != 0
+        if mult_spots.include?(coordinate.vertical) && mult_spots.include?(coordinate.horizontal)
           coordinate.multiple = rand(3) + 1
           if i == 0
             coordinate.multiplier = 'word'
