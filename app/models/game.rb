6288f802e372b1cb5cf053926f53703cec1ca997
Gary Hammell <ghammell1@gmail.com>
2014-12-04 23:13:26 -0800

    update logic in determining the multiplier, necessary for heroku

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,7 +11,7 @@ class Game < ActiveRecord::Base
         coordinate = self.coordinates.create(horizontal: row, vertical: column)
         if mult_spots.include?(coordinate.vertical) && mult_spots.include?(coordinate.horizontal)
           unless count == 112
-            coordinate.multiple = (2..4).to_a.sample
+            coordinate.update_attribute("multiple", (2..4).to_a.sample)
             if i == 0
               coordinate.multiplier = 'W'
               i = 1
