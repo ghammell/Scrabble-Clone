8122aa26d8c387a76496d910346fceda7ac0bbac
Gary Hammell <ghammell1@gmail.com>
2014-12-04 21:30:38 -0800

    update loading of game to exclude multiplier from center coord

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,21 +3,25 @@ class Game < ActiveRecord::Base
   has_many :coordinates
 
   def create_coordinates
-    mult_spots = [0, 3, 7, 10, 14]
+    mult_spots = [0, 3, 7, 11, 14]
+    count = 0
     (0..14).each do |row|
       i = 0
       (0..14).each do |column|
         coordinate = self.coordinates.create(horizontal: row, vertical: column)
         if mult_spots.include?(coordinate.vertical) && mult_spots.include?(coordinate.horizontal)
-          coordinate.multiple = rand(3) + 1
-          if i == 0
-            coordinate.multiplier = 'W'
-            i = 1
-          else
-            coordinate.multiplier = 'L'
-            i = 0
+          unless count == 112
+            coordinate.multiple = (2..4).to_a.sample
+            if i == 0
+              coordinate.multiplier = 'W'
+              i = 1
+            else
+              coordinate.multiplier = 'L'
+              i = 0
+            end
           end
         end
+        count += 1
       end
     end
   end
