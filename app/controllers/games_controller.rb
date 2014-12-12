4c40048bb0c68795e664c9724b2a89fe97151ec1
Gary Hammell <ghammell1@gmail.com>
2014-12-11 19:01:54 -0800

    adjust functionality to build initial hands - limits letters to at most 2 in first hand

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,8 +5,8 @@ class GamesController < ApplicationController
     @game = Game.includes(:coordinates).create
     @coordinates = @game.coordinates.sort_by {|coord| coord.id}
     @start_coordinate = @coordinates[@coordinates.length / 2]
-    @p1_hand = get_hand(7)
-    @p2_hand = get_hand(7)
+    @p1_hand = get_hand(8)
+    @p2_hand = get_hand(8)
     session[:current_word] = []
     session[:player] = 1
     session[:game] = @game.id
@@ -15,6 +15,12 @@ class GamesController < ApplicationController
   def get_hand(num)
     vowels = ['A','E','I','O','U']
     alphabet = ('A'..'Z').to_a - vowels
-    (0..num).map { |num| num < 3 ? vowels.sample : alphabet.sample }
+    hand = []
+    until hand.length == num
+      letter = (num < 3 ? vowels.sample : alphabet.sample)
+      next if hand.count(letter) == 2
+      hand << letter
+    end
+    hand
   end
 end
